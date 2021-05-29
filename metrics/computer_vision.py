c71dc171153834ef6b63ec1003b09ac76b66da26
Jan Hünermann <jan@huenermann.de>
2021-05-30 00:29:56 +0200

    Add option to aggregate AP over multiple images

--- metrics/computer_vision.py
+++ metrics/computer_vision.py
@@ -64,9 +64,10 @@ def generalized_iou(boxes1, boxes2, strict: bool = False, eps: float = 0.):
 
 
 @torch.jit.script
-def precision_recall(labels, sorted_predictions,
+def precision_recall(labels, predictions, confidence,
                      labels_mask: Optional[Tensor] = None,
                      predictions_mask: Optional[Tensor] = None,
+                     image_dim: Optional[int] = None,
                      iou_threshold: float = 0.5):
    """
    Computes the precision-recall curve (P-R curve) given
@@ -74,25 +75,43 @@ def precision_recall(labels, sorted_predictions,
    and labels.
    
    labels: Ground truth bounding boxes with shape [*, n, 4]
-   mask: Booleann mask of labels with shape [*, n]
-   sorted_predictions: Prediction bounding boxes, sorted, with shape [*, m, 4]
-   iou_threshold: Threshold of IOU when to count detection as true positive
+   predictions: Predicted bounding boxes with shape [*, m, 4]
+   confidence: Confidence of each prediction with shape [*, m]
+   labels_mask: Boolean mask of labels with shape [*, n]
+   predictions_mask: Boolean mask of predictions with shape [*, m]
+   image_dim: Dimension of each image (e.g. -2); -1 refers to bbox dimension, therefore it must be different
+   iou_threshold: Threshold of IOU. Predictions with IOU > threshold to labels will be regarded as true positives
    """
-   m = sorted_predictions.size(-2)
+   m = predictions.size(-2)
+   n = int(labels.size(-1))
 
-   _ious = iou(labels[..., :, None, :], sorted_predictions[..., None, :, :])
+   conf_perm = confidence.argsort(-1, descending=True)
+
+   _ious = iou(labels[..., :, None, :], predictions.gather(-2, conf_perm.unsqueeze(-1).expand_as(predictions))[..., None, :, :])
    if labels_mask is not None:
       _ious.masked_fill_(~labels_mask.unsqueeze(-1), 0.)
    if predictions_mask is not None:
-      _ious.masked_fill_(~predictions_mask.unsqueeze(-2), 0.)
+      _ious.masked_fill_(~predictions_mask.gather(-1, conf_perm).unsqueeze(-2), 0.)
    _ious.nan_to_num_(0., posinf=0., neginf=0.)
 
-   trues = _ious >= iou_threshold
+   trues = _ious >= iou_threshold # [*, n, m]
 
    matched = trues.cummax(-1)[0].roll(1, -1)
    matched[..., :, 0] = False
 
-   trues = (trues & (~matched)).max(-2, False)[0]
+   trues = (trues & (~matched)).max(-2, False)[0] # [*, m]
+
+   if image_dim is not None:
+      if image_dim < 0:
+         image_dim += confidence.dim()
+      assert image_dim < confidence.dim() - 1, "Image dim ({}) cannot overlap with bbox dim".format(image_dim)
+      trues = trues.transpose(image_dim, -2).flatten(-2)
+      all_confidences = confidence.gather(-1, conf_perm).transpose(image_dim, -2).flatten(-2)
+      trues = trues.gather(-1, all_confidences.argsort(-1, descending=True))
+      n *= int(labels.size(image_dim))
+      if labels_mask is not None:
+         labels_mask = labels_mask.transpose(image_dim, -2).flatten(-2)
+
    falses = ~trues
 
    tp = trues.float().cumsum(-1)
@@ -101,7 +120,7 @@ def precision_recall(labels, sorted_predictions,
    if labels_mask is not None:
       positives = labels_mask.sum(-1, True)
    else:
-      positives = torch.tensor(labels.size(-1), dtype=labels.dtype, device=labels.device)
+      positives = torch.tensor(n, dtype=labels.dtype, device=labels.device)
 
    recall = tp / positives
    precision = (tp / (tp + fp))
@@ -110,22 +129,27 @@ def precision_recall(labels, sorted_predictions,
 
 
 @torch.jit.script
-def average_precision(labels, sorted_predictions,
+def average_precision(labels, predictions, confidence,
                       labels_mask: Optional[Tensor] = None,
                       predictions_mask: Optional[Tensor] = None,
-                      iou_threshold: float = 0.5, interpolation_points: Optional[int] = None):
+                      image_dim: Optional[int] = None,
+                      iou_threshold: float = 0.5,
+                      interpolation_points: Optional[int] = None):
    """
    Computes the average precision (AP) given
    a ranked list of detections (decreasing probability)
    and labels.
    
    labels: Ground truth bounding boxes with shape [*, n, 4]
-   mask: Booleann mask of labels with shape [*, n]
-   sorted_predictions: Prediction bounding boxes, sorted, with shape [*, m, 4]
-   iou_threshold: Threshold of IOU when to count detection as true positive
-   interpolation_points: Number of interpolation points of P-R-Curve
+   predictions: Predicted bounding boxes with shape [*, m, 4]
+   confidence: Confidence of each prediction with shape [*, m]
+   labels_mask: Boolean mask of labels with shape [*, n] (True for existance of label)
+   predictions_mask: Boolean mask of predictions with shape [*, m] (True for existance of prediction)
+   image_dim: Dimension of each image (e.g. -2); -1 refers to bbox dimension, therefore it must be different
+   iou_threshold: Threshold of IOU. Predictions with IOU > threshold to labels will be regarded as true positives
+   interpolation_points: Number of interpolation points of P-R-Curve. If None, will not interpolate and integrate whole curve (default).
    """
-   precision, recall = precision_recall(labels, sorted_predictions, labels_mask, predictions_mask, iou_threshold)
+   precision, recall = precision_recall(labels, predictions, confidence, labels_mask, predictions_mask, image_dim, iou_threshold)
 
    if interpolation_points is not None:
       recall_points = torch.linspace(0., 1., interpolation_points, device=labels.device, dtype=labels.dtype)\
