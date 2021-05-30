eda74be67c171877be336fc9f58225e3f5850216
Jan Hünermann <jan@huenermann.de>
2021-05-30 03:54:33 +0200

    Update docstrings

--- metrics/computer_vision.py
+++ metrics/computer_vision.py
@@ -93,6 +93,17 @@ def project_rotated_bboxes(labels, predictions):
 
 @torch.jit.script
 def iou_with_format(labels, predictions, bbox_format: str = "xyxy"):
+   """
+   Returns the IOU of label and prediction. The format of the bounding boxes
+   can be specified with the bbox_format parameter, which is either
+   'xyxy' or 'xywha' (includes rotation angle).
+
+   labels: Tensor of shape [*, 4 or 5]
+   predictions: Tensor of shape [*, 4 or 5]
+   bbox_format: Either 'xyxy' or 'xywha'
+
+   Returns IOU in shape [*]
+   """
    assert predictions.size(-1) == labels.size(-1)
    if bbox_format == "xyxy":
       assert labels.size(-1) == 4
@@ -107,13 +118,21 @@ def iou_with_format(labels, predictions, bbox_format: str = "xyxy"):
 
 
 @torch.jit.script
-def get_true_positives(labels, predictions, confidence,
-                       labels_mask: Optional[Tensor] = None,
-                       predictions_mask: Optional[Tensor] = None,
-                       iou_threshold: float = 0.5,
-                       bbox_format: str = "xyxy"):
+def compute_true_positives(labels, predictions, confidence,
+                           labels_mask: Optional[Tensor] = None,
+                           predictions_mask: Optional[Tensor] = None,
+                           iou_threshold: float = 0.5,
+                           bbox_format: str = "xyxy"):
    """
-   Compute true positives
+   Returns tuple of trues (shape [*, m]), positives (shape [*]), and confidence (sorted, shape [*, m]).
+
+   labels: Ground truth bounding boxes with shape [*, n, 4]
+   predictions: Predicted bounding boxes with shape [*, m, 4]
+   confidence: Confidence of each prediction with shape [*, m]
+   labels_mask: Boolean mask of labels with shape [*, n]
+   predictions_mask: Boolean mask of predictions with shape [*, m]
+   iou_threshold: Threshold of IOU. Predictions with IOU > threshold to labels will be regarded as true positives
+   bbox_format: Format of bboxes, either 'xyxy' or 'xywha'
    """
    confidence, perm = confidence.sort(-1, descending=True)
    predictions = predictions.gather(-2, perm.unsqueeze(-1).expand_as(predictions))
@@ -144,9 +163,18 @@ def get_true_positives(labels, predictions, confidence,
 
 
 @torch.jit.script
-def get_precision_recall_from_tp(trues, positives,
-                                 confidence: Optional[Tensor] = None,
+def get_precision_recall_from_tp(trues, positives, confidence: Optional[Tensor] = None,
                                  image_dim: Optional[int] = None):
+   """
+   Computes the precision recall curve from
+   trues (shape [*, m]), positive counts (shape [*]),
+   and confidence (sorted, shape [*, m]).
+
+   trues: Boolean tensor with shape [*, m]
+   positives: Count of positives of shape [*]
+   confidence: Confidence of each prediction with shape [*, m]
+   image_dim: If not none, merges predictions over multiple images together
+   """
    if image_dim is not None:
       assert confidence is not None
       if image_dim < 0:
@@ -162,16 +190,23 @@ def get_precision_recall_from_tp(trues, positives,
       trues = trues.transpose(image_dim, -2).flatten(-2).gather(-1, perm)
 
    tp = trues.float().cumsum(-1)
-   fp = (~trues).float().cumsum(-1)
+   tp_plus_fp = torch.arange(1, tp.size(-1) + 1, device=tp.device, dtype=tp.dtype).expand_as(tp)
 
    recall = tp / positives
-   precision = (tp / (tp + fp))
+   precision = tp / tp_plus_fp
 
    return precision, recall
 
 
 @torch.jit.script
 def get_average_precision_from_pr(precision, recall, interpolation_points: Optional[int] = None):
+   """
+   Computes the average precision (AP) from precision and recall.
+
+   interpolation_points: Optional integer specifying the number of interpolation points (as in VOC Pascal)
+
+   Returns AP tensor with same shape as precision and recall, but with the last dimension reduced.
+   """
    if interpolation_points is not None:
       recall_points = torch.linspace(0., 1., interpolation_points, device=precision.device, dtype=precision.dtype)\
          .expand(recall.shape + (interpolation_points,))
@@ -205,8 +240,9 @@ def precision_recall(labels, predictions, confidence,
    predictions_mask: Boolean mask of predictions with shape [*, m]
    image_dim: Dimension of each image (e.g. -2); -1 refers to bbox dimension, therefore it must be different
    iou_threshold: Threshold of IOU. Predictions with IOU > threshold to labels will be regarded as true positives
+   bbox_format: Format of bboxes, either 'xyxy' or 'xywha'
    """
-   trues, positives, confidence = get_true_positives(
+   trues, positives, confidence = compute_true_positives(
       labels, predictions, confidence, labels_mask, predictions_mask, iou_threshold, bbox_format)
 
    return get_precision_recall_from_tp(trues, positives, confidence, image_dim)
@@ -233,6 +269,7 @@ def average_precision(labels, predictions, confidence,
    image_dim: Dimension of each image (e.g. -2); -1 refers to bbox dimension, therefore it must be different
    iou_threshold: Threshold of IOU. Predictions with IOU > threshold to labels will be regarded as true positives
    interpolation_points: Number of interpolation points of P-R-Curve. If None, will not interpolate and integrate whole curve (default).
+   bbox_format: Format of bboxes, either 'xyxy' or 'xywha'
    """
    precision, recall = precision_recall(labels, predictions, confidence,
       labels_mask, predictions_mask, image_dim, iou_threshold, bbox_format)
