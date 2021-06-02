84d2242823dc134c5c03b2355afce2472bb5f3db
Jan Hünermann <jan@huenermann.de>
2021-06-02 15:57:36 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="alm",
+    version="0.0.1",
+    author="Jan Hünermann",
+    author_email="mail@janhuenermann.com",
+    description="Algorithms for ML",
+    long_description_content_type="text/markdown",
+    url="https://github.com/janhuenermann/alm",
+    project_urls={
+        "Bug Tracker": "https://github.com/janhuenermann/alm/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
